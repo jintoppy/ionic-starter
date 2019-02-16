8a17bbfd612340d046a40bf02faf0c1edcab0a8d
Jinto Jose <jintoppy@gmail.com>
2019-02-16 14:41:44 +0530

    initial

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  isSubmitted = false;
+  onSubmit() {
+    this.isSubmitted = true;
+  }
 }
